e7e300b3b05fd9408a84dfdb32f1e06b3a69d631
AndreyJP <01andreyjesuspereira@gmail.com>
2025-03-08 09:16:33 -0300

    feat:adicionado classe ranking e cadastro de jogadores

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,9 +1,12 @@
 package main;
 
+import jogos.Ranking;
 import model.Jogador;
 
 public class Main {
     public static void main(String[] args) {
+
+        Ranking ranking = new Ranking();
 
         Jogador jogador1 = new Jogador("Maria", 150);
         Jogador jogador2 = new Jogador("João", 200);
@@ -11,5 +14,10 @@ public class Main {
 
         System.out.println(jogador1);
         System.out.println(jogador2);
+        System.out.println(jogador3);
+
+        ranking.adicionarJogador(jogador1);
+        ranking.adicionarJogador(jogador2);
+        ranking.adicionarJogador(jogador3);
     }
 }
